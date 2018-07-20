edbc0d58974bc2d376f26d96b0d564a947c4c41f
JuheonYi <johnyi0606@gmail.com>
2018-07-21 07:57:54 +0900

    180721

---
+++ README.md
@@ -0,0 +1,8 @@
+# VESPCN-PyTorch
+PyTorch implementation of EPSCN [1]/VESPCN [2]
+
+## **TODO list**
+
+[1] W. Shi et al, “Real-time single image and video super-resolution using an efficient sub-pixel convolutional neural network,” IEEE CVPR 2016.
+
+[2] J. Caballero et al, “Real-Time Video Super-Resolution with Spatio-Temporal Networks and Motion Compensation,” IEEE CVPR 2017.
---
+++ models.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ESPCN(nn.Module):
+    def __init__(self, upscale_factor):
+        super(ESPCN, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 64, kernel_size = 5, padding = 4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size = 3, padding = 2)
+        self.conv3 = nn.Conv2d(32, upscale_factor ** 2, kernel_size = 3, padding = 2)
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+        
+        torch.nn.init.xavier_uniform_(self.conv1.weight)
+        torch.nn.init.xavier_uniform_(self.conv2.weight)
+        torch.nn.init.xavier_uniform_(self.conv3.weight)
+        
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.pixel_shuffle(x)
+        x = self.tanh(x)
+        return x
