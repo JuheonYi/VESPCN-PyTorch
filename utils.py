12c0bea8e71cb249c0b59aac16296c4ba05fb7d8
JuheonYi <johnyi0606@gmail.com>
2018-07-26 18:40:48 +0900

    modified utils.py

--- utils.py
+++ utils.py
@@ -10,3 +10,15 @@ def preprocess(img):
 
 def postprocess(img):
     return np.round(np.clip(img*255 + mean_RGB, 0, 255)).astype(np.uint8)
+
+def calc_PSNR(img1, img2):
+    #assume RGB image
+    target_data = np.array(img1, dtype=np.float64)
+    ref_data = np.array(img2,dtype=np.float64)
+    diff = ref_data - target_data
+    diff = diff.flatten('C')
+    rmse = math.sqrt(np.mean(diff ** 2.) )
+    if rmse == 0:
+        return 100
+    else:
+        return 20*math.log10(255.0/rmse)
