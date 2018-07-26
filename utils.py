d78fae6498bac6c038bfeab483a325ab48c29fda
JuheonYi <johnyi0606@gmail.com>
2018-07-26 17:46:09 +0900

    added pre/postprocessing

--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+
+mean_RGB = np.array([123.68 ,  116.779,  103.939])
+
+def preprocess(img):
+    return (img - mean_RGB)/255 
+
+def postprocess(img):
+    return np.round(np.clip(img*255 + mean_RGB, 0, 255)).astype(np.uint8)
