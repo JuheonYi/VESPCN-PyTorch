c4c201173bae78b34109fc42edc61234e0a5069a
Junho Kim <junhokim@Junhos-MacBook-Pro.local>
2018-07-31 02:29:08 +0900

    added comments for test.py

--- test.py
+++ test.py
@@ -2,19 +2,23 @@
 from option import args
 import cv2
 from data.vsrdata import VSRData
-
+"""
+Original test setting: 5 videos with 30 frames inside each directory
+length of frame sequence = 4
+batch_size = 2
+"""
 if __name__ == '__main__':
     vsr = VSRData(args)
-    print(len(vsr.data_hr))
-    print(len(vsr.data_lr))
-    print(vsr.data_hr[1].shape)
-    print(vsr.data_lr[1].shape)
+    print(len(vsr.data_hr))  # 5
+    print(len(vsr.data_lr))  # 5
+    print(vsr.data_hr[1].shape)  # (4,1080,1920,3)
+    print(vsr.data_lr[1].shape)  # (4, 360, 640, 3)
     img_samples = []
     for i in range(args.n_sequence):
         cv2.imwrite('hr_{}.jpg'.format(i), vsr.data_hr[0][i, :])
         cv2.imwrite('lr_{}.jpg'.format(i), vsr.data_lr[0][i, :])
-    print(len(vsr[0][0]))
-    print(vsr[0][0][0].shape)
-    print(len(vsr[0][1]))
-    print(vsr[0][1][0].shape)
-    print(vsr[0][2])
+    print(len(vsr[0][0]))  # 4
+    print(vsr[0][0][0].shape)  # torch.Size([3,17,17])
+    print(len(vsr[0][1]))  # 4
+    print(vsr[0][1][0].shape)  # torch.Size([3,51,51])
+    print(vsr[0][2])  # ['00001', '00002', '00003', '00004']
